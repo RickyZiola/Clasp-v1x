89b0406bef00f947a3aa229ebde7cf2684324e5e
RickyZiola <ziola14227@stu.newlothrop.k12.mi.us>
2023-09-19 10:08:32 -0400

    corrected archi name in compilerICE.c

--- compiler/visitors/compilerICE.c
+++ compiler/visitors/compilerICE.c
@@ -30,18 +30,18 @@ void visitor_literal_int(int val) {
     // This value is NOT loaded into the data segment for faster access times.
     // In the case of string/list/obj literals, they are loaded into the data segment.
 
-    // VirtualICE assembly for loading a constant onto the stack:
+    // ICE assembly for loading a constant onto the stack:
     //               ; Initial,        stack = [ ]
     // const.i <n>   ; Constnant load, stack = [n]
 
-    // TODO: implement VirtualICE bytecode once the instruction set is final
+    // TODO: implement ICE bytecode once the instruction set is final
 }
 void visitor_op_unary(TokenTyp operator_type) {
     switch (operator_type) {
         case TOKEN_MINUS: {
             // TODO: type checking
 
-            // VirtualICE assembly for negating a value on the stack:
+            // ICE assembly for negating a value on the stack:
             //             ; Initial,         stack = [n  ]
             // const.i 0   ; Load a constant, stack = [n 0]
             // swap        ; Swap 2 vals,     stack = [0 n]
@@ -49,18 +49,18 @@ void visitor_op_unary(TokenTyp operator_type) {
 
             // This is equivalent to 0 - n, or just -n
 
-            // TODO: implement VirtualICE bytecode once the instruction set is final
+            // TODO: implement ICE bytecode once the instruction set is final
         } break;
         case TOKEN_BANG: {
             // TODO: type checking
 
-            // VirtualICE assembly for a bitwise NOT:
+            // ICE assembly for a bitwise NOT:
             //              ; Initial,     stack = [n ]
             // not.q        ; Bitwise NOT, stack = [!n]
 
             // This is !n
 
-            // TODO: implement VirtualICE bytecode once the instruction set is final
+            // TODO: implement ICE bytecode once the instruction set is final
         } break;
         default: {
             fprintf(stderr, "Compile error: Unknown unary operator\n");
@@ -75,46 +75,46 @@ void visitor_op_binary(TokenTyp operator_type) {
         case TOKEN_PLUS: {
             // TODO: type checking
 
-            // VirtualICE assembly for adding 2 numbers:
+            // ICE assembly for adding 2 numbers:
             //          ; Initial,  stack = [n m]
             // add.i    ; Addition, stack = [n+m]
 
             // This is n+m
 
-            // TODO: implement VirtualICE bytecode once the instruction set is final
+            // TODO: implement ICE bytecode once the instruction set is final
         } break;
         case TOKEN_MINUS: {
             // TODO: type checking
 
-            // VirtualICE assembly for subtracting 2 numbers:
+            // ICE assembly for subtracting 2 numbers:
             //          ; Initial,     stack = [n m]
             // sub.i    ; Subtraction, stack = [n-m]
 
             // This is n-m
 
-            // TODO: implement VirtualICE bytecode once the instruction set is final
+            // TODO: implement ICE bytecode once the instruction set is final
         } break;
         case TOKEN_ASTERIX: {
             // TODO: type checking
 
-            // VirtualICE assembly for multiplying 2 numbers:
+            // ICE assembly for multiplying 2 numbers:
             //          ; Initial,        stack = [n m]
             // mul.i    ; Multiplication, stack = [n*m]
 
             // This is n*m
 
-            // TODO: implement VirtualICE bytecode once the instruction set is final
+            // TODO: implement ICE bytecode once the instruction set is final
         } break;
         case TOKEN_SLASH: {
             // TODO: type checking
 
-            // VirtualICE assembly for dividing 2 numbers:
+            // ICE assembly for dividing 2 numbers:
             //          ; Initial,  stack = [n m]
             // div.i    ; Division, stack = [n/m]
 
             // This is n/m
 
-            // TODO: implement VirtualICE bytecode once the instruction set is final
+            // TODO: implement ICE bytecode once the instruction set is final
         } break;
         default: {
             fprintf(stderr, "Compile error: unknown binary operator\n");
