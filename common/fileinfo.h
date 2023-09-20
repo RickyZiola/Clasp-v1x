93a1c1db7ef7ac5a0e258ffddbfcc824b0865a67
RickyZiola <ziola14227@stu.newlothrop.k12.mi.us>
2023-09-20 06:55:28 -0400

    Added "LoadedFile" struct and placeholder for ICE build target

--- common/fileinfo.h
+++ common/fileinfo.h
@@ -2,8 +2,12 @@
 #define FILE_INFO_H
 
 #include "../dynabuf/dynabuf.h"
- 
-DynamicBuffer(char *) *loaded_files;
+
+typedef struct {
+
+} LoadedFile;
+
+DynamicBuffer(LoadedFile) *loaded_files;
 
 void setup_files(void);
 
